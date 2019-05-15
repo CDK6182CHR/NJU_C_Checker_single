92672d8545b019dbd5fe0032458f2b0cd94bc45b
CDK6182CHR <794288384@qq.com>
2019-05-15 20:26:30 +0800

    2019.05.15  删除highLighter中多余的空串引起程序未响应问题。Version 2.0.7

--- check_single_gui.py
+++ check_single_gui.py
@@ -24,8 +24,8 @@ class checkWindow(QtWidgets.QMainWindow):
     def __init__(self,parent=None):
         super().__init__()
         self.name = '南京大学C语言作业批改系统'
-        self.version = 'V2.0.6'
-        self.date = '20190512'
+        self.version = 'V2.0.7'
+        self.date = '20190515'
         self.setWindowTitle(f"{self.name} {self.version}")
         self.workDir = '.'
         self.examples = []
